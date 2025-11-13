bb000f15eecf0b31c1490d08e74d187e39255c6b
Dimetrius-eng <mydimka08@gmail.com>
2025-11-13 11:23:23 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,68 +1,68 @@
-// ВЕРСІЯ 44 - Виправлення білого статус-бару (main-menu-screen: transparent)
-const CACHE_NAME = 'it-alias-v44-fix-white-statusbar';
+// ВЕРСІЯ 41 - Виправлений дизайн головного меню
+const CACHE_NAME = 'it-alias-v41-fixed-menu-design';
 
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './words.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png', 
-  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap',
-  './sounds/correct.mp3',
-  './sounds/skip.mp3',
-  './sounds/times-up.mp3',
-  './sounds/tick.mp3'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './words.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png', // Оновлено для використання 512x512
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap',
+  './sounds/correct.mp3',
+  './sounds/skip.mp3',
+  './sounds/times-up.mp3',
+  './sounds/tick.mp3'
 ];
 
 // 1. Подія "install"
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Відкрито кеш v44');
-        const localUrls = urlsToCache.filter(url => !url.startsWith('http'));
-        const externalUrls = urlsToCache.filter(url => url.startsWith('http'));
-        
-        return cache.addAll(localUrls)
-          .then(() => {
-            const externalRequests = externalUrls.map(url => new Request(url, { mode: 'no-cors' }));
-            return Promise.all(externalRequests.map(req => cache.add(req)));
-          });
-      })
-      .catch(err => console.error('Помилка cache.addAll у v44:', err))
-  );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Відкрито кеш v41');
+        const localUrls = urlsToCache.filter(url => !url.startsWith('http'));
+        const externalUrls = urlsToCache.filter(url => url.startsWith('http'));
+        
+        return cache.addAll(localUrls)
+          .then(() => {
+            const externalRequests = externalUrls.map(url => new Request(url, { mode: 'no-cors' }));
+            return Promise.all(externalRequests.map(req => cache.add(req)));
+          });
+      })
+      .catch(err => console.error('Помилка cache.addAll у v41:', err))
+  );
 });
 
 // 2. Подія "fetch"
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request); 
-      })
-  );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request); 
+      })
+  );
 });
 
 // 3. Подія "activate"
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME]; 
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Видалення старого кешу:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-    .then(() => {
-        console.log('Service Worker v44 активовано і перехоплює контроль!');
-        return self.clients.claim();
-    })
-  );
+  const cacheWhitelist = [CACHE_NAME]; 
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Видалення старого кешу:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+    .then(() => {
+        console.log('Service Worker v41 активовано і перехоплює контроль!');
+        return self.clients.claim();
+    })
+  );
 });
